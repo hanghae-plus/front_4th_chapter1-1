9cfe5d4380e47eb5efbcae0bb453e5b71749af0a
chap95 <chap95@likelion.org>
2024-12-15 01:43:31 +0900

    WIP

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,7 +7,6 @@ export const [userName, introduction] = localStorage
 export const initAuth = () => {
   const singLink = document.querySelector("#signLink");
   const profileLink = document.querySelector("#profileLink");
-
   profileLink.parentNode.style.display = userName ? "block" : "none";
   singLink.textContent = userName ? "로그아웃" : "로그인";
   singLink.href = userName ? "/#" : "/login";
