d7fc622e0d449b4bff5e4d42df00b919973b205d
hanmok <dmammmm@naver.com>
2024-12-15 02:28:32 +0900

    split pages

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import { ProfilePage } from "./profile";
+// import { ErrorPage } from "./error";
+// import { LoginPage } from "./login";
+import { MainPage } from "./main";
+
+document.body.innerHTML = `
+  ${MainPage()}
+`;
