f9c274f627d273a37c9fe292f05946f669dfd99e
장원정 <tazoal@tazoal.local>
2024-12-15 01:15:17 +0900

    feat: 기본과제 테스트 - 1. 라우팅 구현 완료

--- src/main.js
+++ src/main.js
@@ -3,9 +3,33 @@ import ProfilePage from "./pages/ProfilePage";
 import LoginPage from "./pages/LoginPage";
 import ErrorPage from "./pages/ErrorPage";
 
-document.body.innerHTML = `
-  ${MainPage()}
-  ${ProfilePage()}
-  ${LoginPage()}
-  ${ErrorPage()}
-`;
+function createRouter(routes) {
+  return function (path) {
+    const route = routes[path] || routes["404"];
+    return route();
+  };
+}
+
+const routes = {
+  "/": () => MainPage(),
+  "/profile": () => ProfilePage(),
+  "/login": () => LoginPage(),
+  404: () => ErrorPage(),
+};
+
+const router = createRouter(routes);
+
+function updateContent() {
+  // 렌더링
+  const path = window.location.pathname;
+  const user = localStorage.getItem("user");
+  if (!user && path === "/profile") {
+    document.body.innerHTML = router("/login");
+    return;
+  }
+
+  document.body.innerHTML = router(path);
+}
+
+window.addEventListener("popstate", updateContent);
+window.addEventListener("load", updateContent);
