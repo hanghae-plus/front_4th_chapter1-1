e5068020c0263d723661bf1c7cd0c1a5c2bc3719
divopsor <divopsor@gmail.com>
2024-12-15 06:56:27 +0900

    test: 테스트 코드 스펙 준수 - E2E 테스트 준수

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,21 @@ import { Router } from "../Router";
 export const Header = () => {
   return `
     <header class="bg-blue-600 text-white p-4 sticky top-0">
-    <h1 class="text-2xl font-bold">항해플러스</h1>
+      <h1 class="text-2xl font-bold">항해플러스</h1>
     </header>
-
-    <nav class="bg-white shadow-md p-2 sticky top-14">
-    <ul class="flex justify-around">
-      <li><a href="/" class="${window.location.pathname === "/" ? "text-blue-600" : "text-gray-600"}">홈</a></li>
-      ${window.location.pathname === "profile" || userService.isLogin() ? `<li><a href="/profile" class="${window.location.pathname === "/profile" ? "text-blue-600" : "text-gray-600"}">프로필</a></li>` : ""}
-      <li><a href="#" class="text-gray-600" id="${userService.isLogin() ? "logout" : "login"}">${userService.isLogin() ? "로그아웃" : "로그인"}</a></li>
-    </ul>
-    </nav>
+    ${
+      window.location.pathname === "/login"
+        ? ""
+        : `
+        <nav class="bg-white shadow-md p-2 sticky top-14">
+        <ul class="flex justify-around">
+          <li><a href="/" class="${window.location.pathname === "/" ? "text-blue-600" : "text-gray-600"}">홈</a></li>
+          ${window.location.pathname === "profile" || userService.isLogin() ? `<li><a href="/profile" class="${window.location.pathname === "/profile" ? "text-blue-600" : "text-gray-600"}">프로필</a></li>` : ""}
+          <li><a href="#" class="text-gray-600" id="${userService.isLogin() ? "logout" : "login"}">${userService.isLogin() ? "로그아웃" : "로그인"}</a></li>
+        </ul>
+        </nav>
+      `
+    }
   `;
 };
 
@@ -23,7 +28,7 @@ Header.init = () => {
     lotoutButton.addEventListener("click", (e) => {
       e.preventDefault();
       userService.logout();
-      Router.push("/");
+      Router.push("/login");
     });
   }
 
