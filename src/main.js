4f080933bd85b06049d8988ecfb82d7d3e4918ab
divopsor <divopsor@gmail.com>
2024-12-15 07:45:05 +0900

    refactor: 렌더링 발생 필요 이벤트 모아 관리

--- src/main.js
+++ src/main.js
@@ -1,36 +1,11 @@
 import { App } from "./App.js";
+import { Renderer } from "./Renderer";
 
-// TODO: Renderer.onDomContentLoaded
 document.addEventListener("DOMContentLoaded", () => {
   App.render();
 });
 
-// TODO: Renderer.onPopState
-window.onpopstate = () => {
-  App.render();
-};
-
-// TODO: Renderer.onPushState / Render.onReplaceState
-["pushState", "replaceState"].forEach((type) => {
-  const originalMethod = window.history[type];
-  window.history[type] = function (state, title, url) {
-    originalMethod.apply(this, arguments);
-    const event = new CustomEvent(type, { detail: { state, title, url } });
-    window.dispatchEvent(event);
-  };
-
-  window.addEventListener(type, () => {
-    App.render();
-  });
-});
-
-// TODO: Renderer.onATagClick
-document.querySelectorAll("a").forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    const url = event.target.getAttribute("href");
-    window.history.pushState({}, "", url);
-    App.render();
-  });
-});
+Renderer.onPopState(() => App.render());
+Renderer.onPushState(() => App.render());
+Renderer.onReplaceState(() => App.render());
+Renderer.onATagClick(() => App.render());
