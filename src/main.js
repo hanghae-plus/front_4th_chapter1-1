2160ac6cd211bcb80042c9d38fa911f04f6ceed2
장원정 <tazoal@tazoal.local>
2024-12-15 01:58:54 +0900

    feat: 기본과제 테스트 2. 사용자 관리 기능, 3. 프로필 페이지 구현

--- src/main.js
+++ src/main.js
@@ -20,14 +20,76 @@ const routes = {
 const router = createRouter(routes);
 
 function updateContent() {
-  // 렌더링
   const path = window.location.pathname;
   const user = localStorage.getItem("user");
   if (!user && path === "/profile") {
-    document.body.innerHTML = router("/login");
+    render("/login");
     return;
   }
 
+  render(path);
+
+  document.body.removeEventListener("submit", submitEventHandler);
+  document.body.addEventListener("submit", submitEventHandler);
+
+  document.body.removeEventListener("click", clickEventHandler);
+  document.body.addEventListener("click", clickEventHandler);
+
+  if (path === "/profile") {
+    const { username, email, bio } = JSON.parse(localStorage.getItem("user"));
+
+    document.getElementById("username").value = username;
+    document.getElementById("email").value = email;
+    document.getElementById("bio").value = bio;
+  }
+}
+
+function submitEventHandler(e) {
+  e.preventDefault();
+  const form = e.target;
+  const formData = new FormData(form);
+  const { id } = form;
+
+  if (id === "login-form") {
+    const username = formData.get("username");
+
+    if (username) {
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ username, email: "", bio: "" }),
+      );
+
+      render("/profile");
+      updateContent();
+    }
+    return;
+  }
+
+  if (id === "profile-form") {
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const bio = formData.get("bio");
+
+    localStorage.setItem("user", JSON.stringify({ username, email, bio }));
+    return;
+  }
+}
+
+function clickEventHandler(e) {
+  const { id } = e.target;
+
+  if (id === "logout") {
+    logout();
+  }
+}
+
+function logout() {
+  localStorage.removeItem("user");
+  render("/login");
+}
+
+function render(path) {
+  window.history.pushState(null, "", path);
   document.body.innerHTML = router(path);
 }
 
