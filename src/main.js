c5e2294702ec1b457c3d0d7d3ca213399f704367
divopsor <divopsor@gmail.com>
2024-12-14 15:28:01 +0900

    feat: 테스트 페이지 제거 후 실제 페이지에 적용

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-import { TestAPage } from "./pages/TestAPage";
-import { TestBPage } from "./pages/TestBPage";
+import { MainPage } from "./pages/MainPage";
+import { ProfilePage } from "./pages/ProfilePage";
+import { LoginPage } from "./pages/LoginPage";
+import { ErrorPage } from "./pages/ErrorPage";
+
 import { SPARouter } from "./SPARouter";
 
 function renderApp() {
-  document.body.innerHTML = `
-    ${SPARouter.pathname === "/test-a" || SPARouter.pathname === "/" ? TestAPage() : ""}
-    ${SPARouter.pathname === "/test-b" ? TestBPage() : ""}
-  `;
+  document.body.innerHTML = (() => {
+    switch (SPARouter.pathname) {
+      case "/":
+        return MainPage();
+      case "/login":
+        return LoginPage();
+      case "/profile":
+        return ProfilePage();
+      default:
+        return ErrorPage();
+    }
+  })();
 
   SPARouter.init({ callback: renderApp });
 }
