87061c3e5ed5e235daf7a5de139822a4bc71b96b
creco-hanghae <creco.hanghae@gmail.com>
2024-12-15 00:22:51 +0900

    pushState / replaceState Event 추가

--- src/SPARouter.js
+++ src/SPARouter.js
@@ -15,21 +15,34 @@ export const SPARouter = {
   callback: null,
   push: (url) => {
     window.history.pushState({}, "", url);
-    SPARouter.callback?.();
   },
 
   replace: (url) => {
     window.history.replaceState({}, "", url);
-    SPARouter.callback?.();
   },
 
   init: ({ callback = () => {} }) => {
     SPARouter.callback = callback;
+
     preventDefaultATagLogic({ callback });
 
     window.onpopstate = () => {
       callback();
     };
+
+    // pushState와 replaceState 오버라이드
+    ["pushState", "replaceState"].forEach((type) => {
+      const originalMethod = window.history[type];
+      window.history[type] = function (state, title, url) {
+        originalMethod.apply(this, arguments);
+        const event = new CustomEvent(type, { detail: { state, title, url } });
+        window.dispatchEvent(event);
+      };
+
+      window.addEventListener(type, () => {
+        callback();
+      });
+    });
   },
 
   get pathname() {
