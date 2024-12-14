7c5c3bd11f23768f55bf80362c2a98d4605e6a00
YeongseoYoon <l112xpla@gmail.com>
2024-12-15 00:45:33 +0900

    feat: 내비게이션 링크에 data-link 속성 추가

--- src/pages/ProfilePage.ts
+++ src/pages/ProfilePage.ts
@@ -8,9 +8,9 @@ const ProfilePage = () => `
 
         <nav class="bg-white shadow-md p-2 sticky top-14">
           <ul class="flex justify-around">
-            <li><a href="/" class="text-gray-600">홈</a></li>
-            <li><a href="/profile" class="text-blue-600">프로필</a></li>
-            <li><a href="#" class="text-gray-600">로그아웃</a></li>
+            <li><a href="/" class="text-gray-600" data-link>홈</a></li>
+            <li><a href="/profile" class="text-blue-600" data-link>프로필</a></li>
+            <li><a href="#" class="text-gray-600" data-link>로그아웃</a></li>
           </ul>
         </nav>
 
