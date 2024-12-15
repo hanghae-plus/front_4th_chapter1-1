53ed3fb77cb0ba5713f76b4e37b459e92daec0fc
YeongseoYoon <btbb@naver.com>
2024-12-15 22:24:50 +0900

    fix: role="navigation" 추가

--- src/shared/components/Header.ts
+++ src/shared/components/Header.ts
@@ -11,14 +11,14 @@ const Header = () => {
     <h1 class="text-2xl font-bold">항해플러스</h1>
   </header>
 
-  <nav class="bg-white shadow-md p-2 sticky top-14">
+  <nav class="bg-white shadow-md p-2 sticky top-14" role="navigation">
   <ul class="flex justify-around">
      <li><a href="/" class="${getNavColor("/")}" data-link>홈</a></li>
      ${
        isLoggedIn
          ? `
        <li><a href="/profile" class="${getNavColor("/profile")}" data-link>프로필</a></li>
-       <li><a id="logout-button" class="${deactiveColor}"  data-action="logout">로그아웃</a></li>
+       <li><a href="#" class="${deactiveColor}" data-action="logout">로그아웃</a></li>
      `
          : `
        <li><a href="/login" class="${getNavColor("/login")}" data-link>로그인</a></li>
