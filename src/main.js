7d9a05c3bcfe94518e36e261568f4ebe9e1a0a90
SU_VIN <pyotnqls25@naver.com>
2024-12-16 01:05:52 +0900

    feat: 로그인 기능 구현

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import { HomePage } from "./pages/HomePage";
 import { LoginPage } from "./pages/LoginPage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { Router } from "./router";
+import { useAuth } from "./store/useAuth";
 
 const route = new Router();
+const auth = new useAuth();
 
 const renderLoginPage = () => {
   document.body.innerHTML = LoginPage();
@@ -46,3 +48,26 @@ document.body.addEventListener("click", (event) => {
     }
   }
 });
+
+// 전역 submit 이벤트 리스너 추가
+document.body.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const form = event.target;
+  const id = event.submitter.id;
+  if (id === "login-form") {
+    loginHandler(form);
+  }
+});
+
+function loginHandler(form) {
+  const idInput = form.querySelector("input[type='text']");
+  const passwordInput = form.querySelector("input[type='password']");
+
+  const id = idInput.value;
+  const password = passwordInput.value;
+
+  const info = { id: id, password: password };
+  auth.login(info);
+  route.navigate("/");
+}
