11f8e7b2afeb185421b44d4c1dfdbedfbfe8ce8d
SU_VIN <pyotnqls25@naver.com>
2024-12-15 22:58:23 +0900

    feat: 네비게이션 활성화 로직 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
-export const Header = () =>
-  `<header class="bg-blue-600 text-white p-4 sticky top-0">
+export const Header = () => {
+  const currentPath = location.pathname;
+  const activeMenu = "text-blue-600";
+  const inActiveMenu = "text-gray-600";
+
+  const isActive = (path) => {
+    return currentPath === path ? activeMenu : inActiveMenu;
+  };
+
+  return `<header class="bg-blue-600 text-white p-4 sticky top-0">
         <h1 class="text-2xl font-bold">항해플러스</h1>
       </header>
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="/" class="text-blue-600">홈</a></li>
-          <li><a href="/profile" class="text-gray-600">프로필</a></li>
+          <li><a href="/" class=${isActive("/")}>홈</a></li>
+          <li><a href="/profile" class=${isActive("/profile")}>프로필</a></li>
           <li><a href="#" class="text-gray-600">로그아웃</a></li>
         </ul>
       </nav>`;
+};
