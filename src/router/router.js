90bf7fecd732a2755e09426994bfe818a6b5cae9
jungeun <jungeun.lee@teamsparta.co>
2024-12-15 16:42:32 +0900

    feat: 라우터 구현

---
+++ src/router/router.js
@@ -0,0 +1,53 @@
+import { MainPage } from "../pages/home";
+import { LoginPage } from "../pages/login";
+import { ProfilePage } from "../pages/profile";
+import { ErrorPage } from "../pages/error";
+
+const routes = {
+  "/": MainPage,
+  "/login": LoginPage,
+  "/profile": ProfilePage,
+};
+
+class Router {
+  constructor(root) {
+    this.root = root;
+    this.init();
+  }
+
+  init() {
+    window.addEventListener("popstate", () => this.handleRoute());
+    document.addEventListener("click", (e) => {
+      if (e.target.matches("[data-link]")) {
+        e.preventDefault();
+        this.navigate(e.target.getAttribute("href"));
+      }
+    });
+    this.handleRoute();
+  }
+
+  handleRoute() {
+    const path = window.location.pathname;
+
+    // 라우트 가드
+    if (path === "/profile" && !localStorage.getItem("user")) {
+      this.navigate("/login");
+      return;
+    }
+
+    if (path === "/login" && localStorage.getItem("user")) {
+      this.navigate("/");
+      return;
+    }
+
+    const page = routes[path] || ErrorPage;
+    this.root.innerHTML = page();
+  }
+
+  navigate(path) {
+    window.history.pushState({}, "", path);
+    this.handleRoute();
+  }
+}
+
+export default Router;
