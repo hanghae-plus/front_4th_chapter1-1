bb114102c75f5b7b1289f73db6c4c249cee4a97f
YeongseoYoon <btbb@naver.com>
2024-12-15 11:23:05 +0900

    feat: 헤더 컴포넌트의 가져오기 방식을 수정하여 구조 분해 할당 사용

--- src/pages/ProfilePage.ts
+++ src/pages/ProfilePage.ts
@@ -1,5 +1,5 @@
 import Footer from "../shared/components/Footer";
-import Header from "../shared/components/Header";
+import { Header } from "../shared/components/Header";
 
 const ProfilePage = () => `
   <div id="root">
