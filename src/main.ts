23e21ddc67f9d267083ef71ec221761af79d7c70
Changyeop Song <waitwait0301@naver.com>
2024-12-17 01:48:50 +0900

    fix: main에서 인스턴스를 전달하면서 사라진 this의 바인딩을 화살표함수로 처리

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { ProfilePage } from "./pages/profile";
 import { Router } from "./utils/router";
 
 const loginPage = new LoginPage(document.querySelector("#root")!);
+const prifilePage = new ProfilePage(document.querySelector("#root")!);
+const mainPage = new MainPage(document.querySelector("#root")!);
 
-Router.addRoute("/", MainPage);
-Router.addRoute("/profile", ProfilePage);
-Router.addRoute("/login", loginPage.render);
+Router.addRoute("/", () => mainPage.render());
+Router.addRoute("/profile", () => prifilePage.render());
+Router.addRoute("/login", () => loginPage.render());
 Router.addRoute("404", ErrorPage);
 Router.init();
