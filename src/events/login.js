1cf4f0f79b8a073f9cebd3e9addca3484bce9e42
bonnie <rilee@jinhakapply.com>
2024-12-16 13:38:23 +0900

    fix: 로그인 버튼과 프로필 업데이트 버튼과의 중복 이슈 수정

--- src/events/login.js
+++ src/events/login.js
@@ -1,17 +1,21 @@
 import { router } from "../router/router.js";
 
 export const handleLogin = () => {
-  const id = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+  const loginBtn = document.querySelector("#login-form");
 
-  const userInfo = { username: "testuser", email: "", bio: "" };
+  if (loginBtn) {
+    const id = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-  if (id && password) {
-    history.pushState({}, "", "/");
-    router();
-    localStorage.setItem("user", JSON.stringify(userInfo));
-  } else {
-    alert("아이디 혹은 비밀번호를 확인해주세요.");
+    const userInfo = { username: "testuser", email: "", bio: "" };
+
+    if (id && password) {
+      history.pushState({}, "", "/");
+      router();
+      localStorage.setItem("user", JSON.stringify(userInfo));
+    } else {
+      alert("아이디 혹은 비밀번호를 확인해주세요.");
+    }
   }
 };
 
