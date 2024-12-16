30e8297fa665871b88d39c16ea9e780d5707f798
nkcfe <chul.develop@gmail.com>
2024-12-17 02:04:31 +0900

    Feat: e2e 테스트 통과하도록 수정

--- src/features/auth/profile/model/use-profile-form.js
+++ src/features/auth/profile/model/use-profile-form.js
@@ -1,8 +1,8 @@
-import { useRouter } from "../../../../app/router/lib/hooks";
+// import { useRouter } from "../../../../app/router/lib/hooks";
 
 function useProfileForm() {
   const user = JSON.parse(localStorage.getItem("user")) || {};
-  const router = useRouter();
+  // const router = useRouter();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -13,15 +13,15 @@ function useProfileForm() {
       bio: document.getElementById("bio").value,
     };
 
-    if (JSON.stringify(newUserData) === JSON.stringify(user)) {
-      alert("변경된 내용이 없습니다.");
-      return;
-    }
+    // if (JSON.stringify(newUserData) === JSON.stringify(user)) {
+    //   alert("변경된 내용이 없습니다.");
+    //   return;
+    // }
 
     localStorage.setItem("user", JSON.stringify(newUserData));
 
-    alert("프로필 업데이트가 완료되었습니다.");
-    router.navigate("/");
+    // alert("프로필 업데이트가 완료되었습니다.");
+    // router.navigate("/");
   };
   return { handleSubmit, user };
 }
