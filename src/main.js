83962344d8132a3efed61889705ec80acdd9d64e
장원정 <tazoal@tazoal.local>
2024-12-17 01:39:58 +0900

    refactor: router 관련 코드 src/router/createRouter.js로 분리

--- src/main.js
+++ src/main.js
@@ -1,52 +1,7 @@
-import MainPage from "./pages/MainPage";
-import ProfilePage from "./pages/ProfilePage";
-import LoginPage from "./pages/LoginPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import UserStore from "./store/userStore";
-
-const routes = {
-  "/": () => MainPage(),
-  "/profile": () => ProfilePage(),
-  "/login": () => LoginPage(),
-  404: () => NotFoundPage(),
-};
-
-const hashRoutes = {
-  "#/": () => MainPage(),
-  "#/profile": () => ProfilePage(),
-  "#/login": () => LoginPage(),
-  404: () => NotFoundPage(),
-};
+import { createRouter } from "@/router/createRouter";
+import UserStore from "@/store/userStore";
 
 const router = createRouter();
-
-function createRouter() {
-  return function (path) {
-    path = path ? path : window.location.pathname;
-    let hash = window.location.hash;
-    let route = null;
-    const user = new UserStore().getUser();
-
-    if (hash === "") {
-      if (!user && path === "/profile") path = "/login";
-      if (user && path === "/login") path = "/";
-      route = routes[path] || routes["404"];
-      window.history.pushState(null, "", path);
-    } else {
-      if (!user && hash === "#/profile") hash = "#/login";
-      if (user && hash === "#/login") hash = "#/";
-      route = hashRoutes[hash] || hashRoutes[404];
-      window.history.pushState(null, "", hash);
-    }
-
-    render(route);
-  };
-}
-
-function render(route) {
-  const root = document.getElementById("root");
-  root.innerHTML = route();
-}
 
 function submitEventHandler(e) {
   e.preventDefault();
--- src/router/createRouter.js
+++ src/router/createRouter.js
@@ -1,20 +1,47 @@
-const createRouter = () => {
-  const callbacks = [];
-  let currentPath = window.location.pathname;
+import MainPage from "@/pages/MainPage";
+import ProfilePage from "@/pages/ProfilePage";
+import LoginPage from "@/pages/LoginPage";
+import NotFoundPage from "@/pages/NotFoundPage";
+import UserStore from "@/store/userStore";
 
-  return {
-    addCallback(callback) {
-      callbacks.push(callback);
-    },
-    navigator(path) {
-      currentPath = path;
-      window.history.pushState(null, "", path);
-      callbacks.forEach((callback) => callback());
-    },
-    getCurrentPath() {
-      return currentPath();
-    },
-  };
+const routes = {
+  "/": () => MainPage(),
+  "/profile": () => ProfilePage(),
+  "/login": () => LoginPage(),
+  404: () => NotFoundPage(),
 };
 
-export default createRouter;
+const hashRoutes = {
+  "#/": () => MainPage(),
+  "#/profile": () => ProfilePage(),
+  "#/login": () => LoginPage(),
+  404: () => NotFoundPage(),
+};
+
+export function createRouter() {
+  return function (path) {
+    path = path ? path : window.location.pathname;
+    let hash = window.location.hash;
+    let route = null;
+    const user = new UserStore().getUser();
+
+    if (hash === "") {
+      if (!user && path === "/profile") path = "/login";
+      if (user && path === "/login") path = "/";
+      route = routes[path] || routes["404"];
+      window.history.pushState(null, "", path);
+    } else {
+      if (!user && hash === "#/profile") hash = "#/login";
+      if (user && hash === "#/login") hash = "#/";
+      route = hashRoutes[hash] || hashRoutes[404];
+      window.history.pushState(null, "", hash);
+    }
+
+    render(route);
+  };
+}
+
+function render(route) {
+  const root = document.getElementById("root");
+  root.innerHTML = route();
+}
