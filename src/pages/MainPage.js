d57373cffdc3aa605271810a53a6216206320c1a
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2024-12-16 21:08:48 +0900

    fix: 오류 수정
    - font 오류 수정
    - 띄어쓰기 수정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -37,13 +37,13 @@ const MainTemplate = (isLogin) => `
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="/" class="text-blue-600">홈</a></li>
+          <li><a href="/" class="text-blue-600 font-bold">홈</a></li>
           ${
             isLogin
               ? `<li><a href="/profile" class="text-gray-600">프로필</a></li>
-<li><button id="logout"  class="text-gray-600">로그아웃</button></li>
+<li><button id="logout" class="text-gray-600">로그아웃</button></li>
 `
-              : `<li><a href="/login"  class="text-gray-600">로그인</a></li>`
+              : `<li><a href="/login" class="text-gray-600">로그인</a></li>`
           }
         </ul>
       </nav>
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -58,7 +58,7 @@ export const ProfileTemplate = () => `
         <nav class="bg-white shadow-md p-2 sticky top-14">
           <ul class="flex justify-around">
             <li><a href="/" class="text-gray-600">홈</a></li>
-            <li><a href="/profile" class="text-blue-600">프로필</a></li>
+            <li><a href="/profile" class="text-blue-600 font-bold">프로필</a></li>
             <li><button id="logout" class="text-gray-600">로그아웃</button></li>
           </ul>
         </nav>
