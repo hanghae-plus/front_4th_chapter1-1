cb4a9dafef7afe248a1f10d97e5a1cb804bb438b
장원정 <tazoal@tazoal.local>
2024-12-17 01:59:47 +0900

    refator: 일반 라우터와 해시 라우터 분리, path를 받아서 페이지를 렌더링하는 navigator 함수 추가

--- src/router/createRouter.js
+++ src/router/createRouter.js
@@ -4,44 +4,53 @@ import LoginPage from "@/pages/LoginPage";
 import NotFoundPage from "@/pages/NotFoundPage";
 import UserStore from "@/store/userStore";
 
-const routes = {
-  "/": () => MainPage(),
-  "/profile": () => ProfilePage(),
-  "/login": () => LoginPage(),
-  404: () => NotFoundPage(),
-};
-
-const hashRoutes = {
-  "#/": () => MainPage(),
-  "#/profile": () => ProfilePage(),
-  "#/login": () => LoginPage(),
-  404: () => NotFoundPage(),
-};
-
 export function createRouter() {
-  return function (path) {
-    path = path ? path : window.location.pathname;
-    let hash = window.location.hash;
-    let route = null;
-    const user = new UserStore().getUser();
+  const ROUTES = {
+    "/": () => MainPage(),
+    "/profile": () => ProfilePage(),
+    "/login": () => LoginPage(),
+    404: () => NotFoundPage(),
+  };
 
-    if (hash === "") {
+  const HASH_ROUTES = {
+    "#/": () => MainPage(),
+    "#/profile": () => ProfilePage(),
+    "#/login": () => LoginPage(),
+    404: () => NotFoundPage(),
+  };
+
+  function render(route) {
+    const root = document.getElementById("root");
+    root.innerHTML = route();
+  }
+
+  return {
+    router() {
+      let path = window.location.pathname;
+      const user = new UserStore().getUser();
+
       if (!user && path === "/profile") path = "/login";
       if (user && path === "/login") path = "/";
-      route = routes[path] || routes["404"];
+      const route = ROUTES[path] || ROUTES["404"];
       window.history.pushState(null, "", path);
-    } else {
+      render(route);
+    },
+    hashRouter() {
+      let hash = window.location.hash;
+      const user = new UserStore().getUser();
       if (!user && hash === "#/profile") hash = "#/login";
       if (user && hash === "#/login") hash = "#/";
-      route = hashRoutes[hash] || hashRoutes[404];
+      const route = HASH_ROUTES[hash] || HASH_ROUTES[404];
       window.history.pushState(null, "", hash);
-    }
-
-    render(route);
+      render(route);
+    },
+    navigator(path) {
+      const user = new UserStore().getUser();
+      if (!user && path === "/profile") path = "/login";
+      if (user && path === "/login") path = "/";
+      const route = ROUTES[path] || ROUTES["404"];
+      window.history.pushState(null, "", path);
+      render(route);
+    },
   };
-}
-
-function render(route) {
-  const root = document.getElementById("root");
-  root.innerHTML = route();
 }
