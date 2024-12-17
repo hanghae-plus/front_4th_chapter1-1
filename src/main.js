aabc5eb1a918ff27c1fcb6585ae712774afa56e2
WonYoung <wy.heo@teamsparta.co>
2024-12-18 01:06:09 +0900

    feat: 프로필 업데이트 추가

--- src/main.js
+++ src/main.js
@@ -29,10 +29,58 @@ app();
 
 $("#root").addEventListener("click", (event) => {
   const router = new Router();
+  const target = event.target;
+
+  if (target.id === "logout") {
+    event.preventDefault(); // 기본 동작 막기
+    localStorage.removeItem("user");
+    router.push("/");
+    return;
+  }
+
   if (event.target.tagName === "A") {
     event.preventDefault(); // 기본 동작 막기
 
     const path = event.target.getAttribute("href");
     router.push(path);
+    return;
+  }
+});
+
+$("#root").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const router = new Router();
+  const target = event.target;
+  const formData = new FormData(target);
+
+  switch (target.id) {
+    case "login-form":
+      {
+        const { username } = Object.fromEntries(formData);
+        if (username) {
+          const user = {
+            username,
+            email: "",
+            bio: "",
+          };
+          localStorage.setItem("user", JSON.stringify(user));
+
+          router.push("/profile");
+        }
+      }
+      break;
+    case "profile-form":
+      {
+        const { username, email, bio } = Object.fromEntries(formData);
+        if (username) {
+          const user = {
+            username,
+            email,
+            bio,
+          };
+          localStorage.setItem("user", JSON.stringify(user));
+        }
+      }
+      break;
   }
 });
