18fe2062c0dd1f37007b8a9248281b9b8c25cf25
divopsor <divopsor@gmail.com>
2024-12-18 03:34:07 +0900

    refactor: 불필요한 CustomEvent 사용 제거

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,26 +1,24 @@
-function overrideHistoryMethod(method) {
-  const originalMethod = window.history[method];
-  window.history[method] = function (state, title, url) {
-    originalMethod.apply(this, arguments);
-    const event = new CustomEvent(method, { detail: { state, title, url } });
-    window.dispatchEvent(event);
-  };
-}
-
 export const Renderer = {
   onPopState: (callback) => {
     window.addEventListener("popstate", callback);
   },
+
   onPushState: (callback) => {
-    overrideHistoryMethod("pushState");
-
-    window.addEventListener("pushState", callback);
+    const originalPushState = window.history.pushState;
+    window.history.pushState = function (state, title, url) {
+      originalPushState.apply(this, arguments);
+      callback(state, title, url);
+    };
   },
+
   onReplaceState: (callback) => {
-    overrideHistoryMethod("onReplaceState");
-
-    window.addEventListener("onReplaceState", callback);
+    const originalReplaceState = window.history.replaceState;
+    window.history.replaceState = function (state, title, url) {
+      originalReplaceState.apply(this, arguments);
+      callback(state, title, url);
+    };
   },
+
   onATagClick: (callback) => {
     document.querySelectorAll("a").forEach((link) => {
       link.addEventListener("click", (event) => {
