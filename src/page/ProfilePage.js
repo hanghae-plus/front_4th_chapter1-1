ec88e528dd241db669b28b3a5e26f35d2de627ed
Dayglow0926 <dayglow0926@gmail.com>
2024-12-18 00:29:15 +0900

    remove: console.log 삭제

--- src/page/ProfilePage.js
+++ src/page/ProfilePage.js
@@ -21,8 +21,6 @@ class ProfilePage {
 
     const store = this.store.getState();
 
-    console.log(store);
-
     this.root.innerHTML = `
     <div class="bg-gray-100 min-h-screen flex justify-center">
       <div class="max-w-md w-full">
