631674e9cf5c0b32fa4581c509208ce0e207660e
anne-hyeyeon <khy2106@naver.com>
2024-12-18 01:49:46 +0900

    fix: DOMContentLoaded 이벤트 리스너 삭제

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { router } from "./lib/router.js";
 import { setupEventHandlers } from "./lib/eventHandlers.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  router.init("root");
-  setupEventHandlers();
-});
+router.init("root");
+setupEventHandlers();
