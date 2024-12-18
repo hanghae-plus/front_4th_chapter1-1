b7e59b580159c0ec4fe65e4f38423fe802b20c30
YeongseoYoon <btbb@naver.com>
2024-12-18 23:34:31 +0900

    feat: 전역 상태 관리를 위한 Store 클래스 추가

---
+++ src/core/Store.ts
@@ -0,0 +1,32 @@
+class Store<T> {
+  private key: string;
+  private value: T;
+
+  constructor(key: string) {
+    this.key = key;
+    const stored = localStorage.getItem(this.key) || "{}";
+    this.value = JSON.parse(stored);
+  }
+
+  get(): T {
+    const stored = localStorage.getItem(this.key) || "{}";
+    this.value = JSON.parse(stored);
+    return this.value;
+  }
+
+  set(value: T): void {
+    localStorage.setItem(this.key, JSON.stringify(value));
+    this.value = value;
+  }
+
+  clear(): void {
+    localStorage.removeItem(this.key);
+    this.value = {
+      username: "",
+      email: "",
+      bio: "",
+    } as T;
+  }
+}
+
+export default Store;
