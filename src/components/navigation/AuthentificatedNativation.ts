81b565577716f3536e3bcc85662277f1a1547d94
Changyeop Song <waitwait0301@naver.com>
2024-12-19 02:05:42 +0900

    fix: advanced 통과를 위한 홈 클래스네임 추가

--- src/components/navigation/AuthentificatedNativation.ts
+++ src/components/navigation/AuthentificatedNativation.ts
@@ -3,7 +3,7 @@ export class AuthentificatedNavigation {
     return `     
      <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="/" name="/" class="text-blue-600">홈</a></li>
+          <li><a href="/" name="/" class="text-blue-600 font-bold">홈</a></li>
           <li><a href="/profile" name="/profile" class="text-gray-600">프로필</a></li>
           <li><a id="logout" href="/login" name="logout" class="text-gray-600">로그아웃</a></li>
         </ul>
