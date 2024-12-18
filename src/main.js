a4d433df562b549076fdd5cf3844b1d8d521e2f3
confidential-nt <youyoy689@gmail.com>
2024-12-18 18:53:44 +0900

    feat: 심화 과제 구현

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import browserRouter from "./router/browser-router";
+import hashRouter from "./router/hash-router";
+
+if (location.hash) {
+  hashRouter(location.hash.slice(1));
+}
 
 browserRouter(location.pathname);
 
 window.addEventListener("popstate", () => {
   browserRouter(location.pathname, false);
+});
+
+window.addEventListener("hashchange", () => {
+  hashRouter(location.hash.slice(1));
 });
