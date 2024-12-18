20af9a659bc45d59b93f03232f9a02a096f79067
oreo <93peace@naver.com>
2024-12-18 11:07:08 +0900

    전체 테스트 통과 버전

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,25 @@
-export const Header = (isLoggedIn) => /*html*/ `  
+const navigationData = [
+  {
+    linkUrl: "/",
+    text: "홈",
+  },
+  {
+    linkUrl: "/profile",
+    text: "프로필",
+  },
+];
+
+export const Header = (isLoggedIn) => {
+  const path = window.location.pathname;
+  return /*html*/ `  
     <header class="bg-blue-600 text-white p-4 sticky top-0">
         <h1 class="text-2xl font-bold">항해플러스</h1>
     </header>
     <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-            <li><a href="/" class="text-blue-600">홈</a></li>
-            <li><a href="/profile" class="text-gray-600">프로필</a></li>
+            ${navigationData?.map((e) => {
+              return `<li><a href="${e.linkUrl}" class="text-blue-600 ${path === e.linkUrl ? "font-bold" : ""}">${e.text}</a></li>`;
+            })}
             ${
               isLoggedIn
                 ? `<li>
@@ -22,3 +36,4 @@ export const Header = (isLoggedIn) => /*html*/ `
         </ul>
     </nav>
 `;
+};
