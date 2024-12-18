5f94c356508e9ff71c27c736bb29f00885792203
CodyMan0 <hys83751952@gmail.com>
2024-12-18 21:42:08 +0900

    :bug: login 페이지 테스트 통과를 위한 Text 변경

--- src/pages/login/login-page.js
+++ src/pages/login/login-page.js
@@ -4,14 +4,14 @@ import { MyRouter } from "../../shared/router/router";
 const LoginPage = () => {
   return `
   <main class="bg-gray-100 flex items-center justify-center min-h-screen">
-    <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-      <h1 class="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>
+  <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+    <h1 aria-role="heading" class="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>
       <form id="login-form">
         <div class="mb-4">
-          <input id="username" name="username" type="text" placeholder="유저 네임" class="w-full p-2 border rounded">
+          <input id="username" type="text" placeholder="사용자 이름" class="w-full p-2 border rounded">
         </div>
         <div class="mb-6">
-          <input id="password" name="password" type="password" placeholder="비밀번호" class="w-full p-2 border rounded">
+          <input type="password" placeholder="비밀번호" class="w-full p-2 border rounded">
         </div>
         <button type="submit" class="w-full bg-blue-600 text-white p-2 rounded font-bold">로그인</button>
       </form>
@@ -20,7 +20,7 @@ const LoginPage = () => {
       </div>
       <hr class="my-6">
       <div class="text-center">
-        <button lass="bg-green-500 text-white px-4 py-2 rounded font-bold">새 계정 만들기</button>
+        <button class="bg-green-500 text-white px-4 py-2 rounded font-bold">새 계정 만들기</button>
       </div>
     </div>
   </main>
