5d45cf68e8223ac46bf5e748992c272860871caa
Jaehyun Bang <jaehyunbang@Jaehyuns-MacBook-Air.local>
2024-12-19 11:25:05 +0900

    feat : Validate 함수 생성

---
+++ src/utils/validate.js
@@ -0,0 +1,23 @@
+import { HASH_ROUTE_DATA, ROUTE_DATA, ROUTE_PATH } from "../constants/Route";
+import ErrorPage from "../pages/ErrorPage";
+import { getLocalStorage } from "./storage";
+const Validate = {
+  path(checkData) {
+    if (!this.isLogin()) return ROUTE_DATA[ROUTE_PATH.LOGIN];
+    const data = ROUTE_DATA.find((route) => route.path === checkData);
+    return data ? data : ROUTE_DATA[ROUTE_PATH.ERROR];
+  },
+
+  hash(checkData) {
+    const data = HASH_ROUTE_DATA.find((route) => route.path === checkData);
+    return data ? data : { path: "/404", view: ErrorPage() };
+  },
+
+  isLogin() {
+    if (location.pathname === "/profile" && !getLocalStorage("user"))
+      return false;
+    return true;
+  },
+};
+
+export default Validate;
