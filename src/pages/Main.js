f2fb87f1ad106e71aa786f3356f75c12a1f08387
Bae seong-kyu <baesense@naver.com>
2024-12-19 23:27:24 +0900

    Fix : 테스트가 통과되지 않는 문제

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,5 @@
-import Footer from "../components/footer";
-import Header from "../components/header";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 import browserRouter from "../router/browser-router";
 import { removeUser } from "../utils/user";
 
