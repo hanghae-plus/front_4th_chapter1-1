46b96178ab2266e6c95dc321abb1e6ffc0527a90
Jaehyun Bang <jaehyunbang@Jaehyuns-MacBook-Air.local>
2024-12-19 13:45:57 +0900

    fix : nav item focus시, 스타일 적용되도록 수정

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,16 @@
 import { getLocalStorage } from "../utils/storage";
 const NavBar = () => {
   const user = JSON.parse(getLocalStorage("user"));
+  const profileNavItem =
+    location.pathname === "/profile"
+      ? `<li><a href="/profile" data-link class="text-blue-600 font-bold">프로필</a></li>`
+      : `<li><a href="/profile" data-link class="text-gray-600">프로필</a></li>`;
   return `
     <nav class="bg-white shadow-md p-2 sticky top-14">
         <ul class="flex justify-around">
-          <li><a href="/" data-link class="text-blue-600">홈</a></li>
-          ${user ? '<li><a href="/profile" data-link class="text-gray-600">프로필</a></li>' : ""}
-          <li><a id="${user ? "logout" : "login"}" href="/login" data-link class="text-gray-600">${user ? "로그아웃" : "로그인"}</a></li>
+          <li><a href="/" data-link class='${location.pathname === "/" ? "text-blue-600 font-bold" : "text-gray-600"}'>홈</a></li>
+          ${user ? profileNavItem : ""}
+          <li><a id="${user ? "logout" : "login"}" href="/login" data-link class=${location.pathname === "/login" ? "text-blue-600 font-bold" : "text-gray-600"}>${user ? "로그아웃" : "로그인"}</a></li>
         </ul>
       </nav>    
     `;
