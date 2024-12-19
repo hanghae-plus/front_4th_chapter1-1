df0a75208972ed8d530400a19d62d97af781220b
Jaehyun Bang <jaehyunbang@Jaehyuns-MacBook-Air.local>
2024-12-19 13:48:04 +0900

    fix : 로그인 한 사용자가 /login 접근 시, 메인페이지로 리다이렉션 하도록 수정

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,9 +3,10 @@ import ErrorPage from "../pages/ErrorPage";
 import { getLocalStorage } from "./storage";
 const Validate = {
   path(checkData) {
-    if (!this.isLogin()) return ROUTE_DATA[ROUTE_PATH.LOGIN];
     if (location.pathname === "/profile" && !this.isLogin())
       return ROUTE_DATA[ROUTE_PATH.LOGIN];
+    if (location.pathname === "/login" && this.isLogin())
+      return ROUTE_DATA[ROUTE_PATH.MAIN];
     const data = ROUTE_DATA.find((route) => route.path === checkData);
     return data ? data : ROUTE_DATA[ROUTE_PATH.ERROR];
   },
