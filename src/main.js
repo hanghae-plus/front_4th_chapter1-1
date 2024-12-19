0b5fc0d53339223b11856ac353a89f6f5c01a0a1
fdgh134 <fdgh134@naver.com>
2024-12-19 14:05:19 +0900

    ing

--- src/main.js
+++ src/main.js
@@ -379,20 +379,15 @@ const router = () => {
   document.body.innerHTML = page;
 
   if (path !== '/login' && path !== '/404') {
-    renderNav();
     renderHeader();
   }
-
+  renderNav();
   pageEventListeners();
 };
 
 const renderHeader = () => {
-  const header = document.querySelectorAll('header');
+  const header = document.querySelector('header');
   if (!header) return;
-
-  header.innerHTML = `
-    <h1 class="text-2xl font-bold">항해플러스</h1>
-  `;
 };
 
 // navigation render
