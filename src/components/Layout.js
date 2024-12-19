d5f72e871698c24be3c9ec348eec362bf2b97387
Jaehyun Bang <jaehyunbang@Jaehyuns-MacBook-Air.local>
2024-12-19 11:30:16 +0900

    refactor : Router data에서 view 반환값 함수로 변경

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ const Layout = (element) => {
   <div class="bg-gray-100 min-h-screen flex justify-center">
     <div class="max-w-md w-full">
     ${element.path === "/" || element.path === "/profile" ? Header() + NavBar() : ""}
-    ${element.view}      
+    ${element.view()}      
     ${element.path === "/" || element.path === "/profile" ? Footer() : ""}
     </div>
   </div>    
--- src/constants/Route.js
+++ src/constants/Route.js
@@ -3,9 +3,18 @@ import LoginPage from "../pages/LoginPage";
 import ProfilePage from "../pages/ProfilePage";
 import ErrorPage from "../pages/ErrorPage";
 
+export const ROUTE_PATH = { MAIN: 0, LOGIN: 1, PROFILE: 2, ERROR: 3 };
+
 export const ROUTE_DATA = [
-  { path: "/", view: MainPage() },
-  { path: "/login", view: LoginPage() },
-  { path: "/profile", view: ProfilePage() },
-  { path: "/404", view: ErrorPage() },
+  { path: "/", view: () => MainPage() },
+  { path: "/login", view: () => LoginPage() },
+  { path: "/profile", view: () => ProfilePage() },
+  { path: "/404", view: () => ErrorPage() },
+];
+
+export const HASH_ROUTE_DATA = [
+  { path: "#/", view: MainPage() },
+  { path: "#/login", view: LoginPage() },
+  { path: "#/profile", view: () => ProfilePage() },
+  { path: "#/404", view: ErrorPage() },
 ];
